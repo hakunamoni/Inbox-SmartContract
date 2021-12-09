da5d022afaad7ac6b03915f555ee866d17f95b3b
hakunamoni <hakunamoni@gmail.com>
2021-12-09 23:27:14 +0800

    deploy update

--- deploy.js
+++ deploy.js
@@ -14,12 +14,12 @@ const deploy = async () => {
 
     console.log('Attempting to deploy from account', accounts[0]);
 
-    // const result = await new web3.eth.Contract(JSON.parse(interface))
-    //     .deploy({ data: bytecode, arguments:['Hi, Rinkeby Contract']})
-    //     // .deploy({ data: '0x' + bytecode, arguments:['Hi, Rinkeby Contract']})
-    //     .send({ from: accounts[0], gas: '1000000'});
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments:['Hi, Rinkeby Contract']})
+        // .deploy({ data: '0x' + bytecode, arguments:['Hi, Rinkeby Contract']})
+        .send({ from: accounts[0], gas: '1000000'});
 
-    // console.log('Contract deployed to', result.options.address);
+    console.log('Contract deployed to', result.options.address);
 };
 deploy();
 // here, deploy function is only just to call async() function and it doesn't have no other meaning
